813687bff2ce2f637db6bf331c0586fc153c15db
Ibraz <ibrazsiddique424@gmail.com>
2025-07-07 21:27:47 +0500

    Update header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -219,7 +219,7 @@ function Header() {
                     {/* Desktop Actions */}
                     <div className="hidden lg:flex items-center space-x-4">
                         {/* <ThemeToggle /> */}
-                        <div className="w-px h-7 bg-gray-300"></div>
+                        {/* <div className="w-px h-7 bg-gray-300"></div> */}
                         {isAuthenticated ? (
                             <div className="relative" ref={dropdownRef}>
                                 <button
