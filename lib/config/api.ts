27dec0b661328b11a4c6c4e35767ec50d3c8e69f
WaqarKhan <waqarkhanmps@hotmail.com>
2025-07-25 19:06:24 +0500

    Discord work

--- lib/config/api.ts
+++ lib/config/api.ts
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-  BASE_URL:  'http://127.0.0.1:4004', // 'https://api.32beatwriters.staging.pegasync.com', //process.env.NEXT_PUBLIC_API_BASE_URL ||
+  BASE_URL:  'http://localhost:4004', // 'https://api.32beatwriters.staging.pegasync.com', //process.env.NEXT_PUBLIC_API_BASE_URL ||
   ENDPOINTS: {
     ARTICLES: '/api/articles',
     AUTH: '/api/users',
