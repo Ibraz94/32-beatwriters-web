de4004613646125f9ef7e7b274e171b612ba9d7b
WaqarKhan <waqarkhanmps@hotmail.com>
2025-08-07 23:29:51 +0500

    UI changes & responsiveness

--- app/components/TrustedByComponent.tsx
+++ app/components/TrustedByComponent.tsx
@@ -3,47 +3,47 @@ import Image from "next/image";
 const TrustedByPartners = () => {
   return (
     <section className="container mx-auto mt-4 p-2">
-      <div className="text-center bg-gray-50">
-        <h2 className="text-3xl sm:text-4xl lg:text-5xl font-semibold text-gray-800 mb-4 pt-3 sm:pt-5 font-oswald">Trusted By</h2>
-        <div className="grid grid-cols-4 sm:flex sm:flex-wrap justify-center gap-0 sm:gap-6">
+      <div className="text-center">
+        <h2 className="text-3xl sm:text-4xl lg:text-5xl font-semibold text-foreground mb-4 pt-3 sm:pt-5 font-oswald">Trusted By</h2>
+        <div className="grid grid-cols-2 gap-4 justify-center max-w-xs mx-auto min-[435px]:flex min-[435px]:max-w-none">
           {/* Logo 1 */}
-          <div className="w-24 h-24 sm:w-32 sm:h-32 flex justify-center items-center">
+          <div className="w-24 h-20 sm:w-32 sm:h-20 px-2 bg-gray-200 flex justify-center items-center rounded-lg max-[435px]:w-full">
             <Image
               src="/ESPN_logo.png"
               alt="ESPN Logo"
               width={120}
               height={120}
-              className="w-20 h-20 sm:w-32 sm:h-32 object-contain"
+              className="object-contain max-w-full"
             />
           </div>
           {/* Logo 2 */}
-          <div className="w-24 h-24 sm:w-32 sm:h-32 flex justify-center items-center">
+          <div className="w-24 h-20 sm:w-32 sm:h-20 px-2 bg-gray-200 flex justify-center items-center rounded-lg max-[435px]:w-full">
             <Image
               src="/CBS_logo.png"
               alt="CBS Logo"
               width={120}
               height={120}
-              className="w-20 h-20 sm:w-32 sm:h-32 object-contain"
+              className="object-contain max-w-full"
             />
           </div>
           {/* Logo 3 */}
-          <div className="w-24 h-24 sm:w-32 sm:h-32 flex justify-center items-center">
+          <div className="w-24 h-20 sm:w-32 sm:h-20 px-2 bg-gray-200 flex justify-center items-center rounded-lg max-[435px]:w-full">
             <Image
               src="/NBC_logo.png"
               alt="NBC Logo"
               width={120}
               height={120}
-              className="w-20 h-20 sm:w-32 sm:h-32 object-contain"
+              className="object-contain max-w-full"
             />
           </div>
           {/* Logo 4 */}
-          <div className="w-24 h-24 sm:w-32 sm:h-32 flex justify-center items-center">
+          <div className="w-24 h-20 sm:w-32 sm:h-20 px-2 bg-gray-200 flex justify-center items-center rounded-lg max-[435px]:w-full">
             <Image
               src="/sleeper_logo.png"
               alt="Sleeper Logo"
               width={150}
               height={150}
-              className="w-20 h-20 sm:w-32 sm:h-32 object-contain"
+              className="object-contain max-w-full"
             />
           </div>
         </div>
