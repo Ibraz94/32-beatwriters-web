1e1c1dda3e4ff0225666e5e64817407dbcb94f78
Ibraz <ibrazsiddique424@gmail.com>
2025-08-18 23:38:34 +0500

    Add Call To Action
    On premium articles for none users add CTA card after some preview content.

--- app/(pages)/articles/page.tsx
+++ app/(pages)/articles/page.tsx
@@ -101,7 +101,7 @@ export default function ArticlesPage() {
   // Helper function to get button configuration
   const getButtonConfig = (article: any) => {
     // console.log('Article Access inside getButtonConfig', article.access);
-    const canAccess = canAccessArticle(article.access)
+    const canAccess = getAccessStatusText(article.access)
 
     if (canAccess) {
       return {
@@ -250,7 +250,7 @@ export default function ArticlesPage() {
         <div dangerouslySetInnerHTML={{ __html: article.content }} />
       </div>
 
-     <div className="absolute bottom-0 left-0 w-full h-44 bg-gradient-to-t from-[#1A1330] to-transparent z-20 pointer-events-none" />
+     <div className="absolute bottom-0 left-0 w-full h-44 bg-gradient-to-t from-[#1A1330] to-transparent z-20 pointer-events-none" /> 
 
       {/* Hover Action Button */}
       <div className="absolute bottom-0 left-0 w-full p-3 z-30 opacity-0 group-hover:opacity-100 transition-opacity">
