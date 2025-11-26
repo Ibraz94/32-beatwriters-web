df7eab6d567ace353aba02cc87571b115e824d10
Ibraz <ibrazsiddique424@gmail.com>
2025-11-26 18:54:00 +0500

    Bug fixes

--- app/components/ArticlesCTA.tsx
+++ app/components/ArticlesCTA.tsx
@@ -5,15 +5,15 @@ import { usePathname } from 'next/navigation'
 const ArticleCTA = () => {
   const pathname = usePathname();
   return (
-    <div className="header-secondary border-2 rounded-xl p-5 text-center bg-white bg-opacity-10 backdrop-blur-lg shadow-lg mb-5">
-      <h2 className="text-white text-2xl font-semibold mb-4">
+    <div className="border-2 rounded-xl p-5 text-center bg- bg-opacity-10 backdrop-blur-lg shadow-xl mb-5">
+      <h2 className="text-2xl font-semibold mb-4">
         You're Reading the Free Article! Unlock Premium Articles with 32BeatWriters.com 
       </h2>
       <div className="space-y-4">
         {/* Subscribe Button */}
         <Link
           href="/subscribe"
-          className="bg-red-800 hover:scale-102 text-white px-6 mr-2 py-1 rounded text-md transition-all duration-200 shadow-sm hover:shadow-md"
+          className="bg-[#E64A30] hover:scale-102 text-white px-6 mr-2 py-1 rounded-full text-md transition-all duration-200 shadow-sm hover:shadow-md"
         >
           Subscribe
         </Link>
@@ -24,7 +24,7 @@ const ArticleCTA = () => {
         pathname: '/login',
         query: { redirect: pathname }  // Pass the current path as a query parameter
       }}
-          className="desktop-login-link hover:scale-102 transition-colors text-md"
+          className="text-md hover:text-[#E64A30]"
         >
           Login
         </Link>
